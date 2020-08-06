0274e6621616137d8ce0aa7dc3ece13cd0b9112e
Aliaksandr Valialkin <valyala@gmail.com>
2020-08-06 23:00:08 +0300

    Add VisitAll() function for visiting all the subexpressions

--- utils_test.go
+++ utils_test.go
@@ -41,3 +41,26 @@ func TestExpandWithExprsError(t *testing.T) {
 	f(``)
 	f(`  with (`)
 }
+
+func TestVisitAll(t *testing.T) {
+	f := func(q, sExpected string) {
+		t.Helper()
+		expr, err := Parse(q)
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		var buf []byte
+		VisitAll(expr, func(e Expr) {
+			buf = e.AppendString(buf)
+			buf = append(buf, ',')
+		})
+		if string(buf) != sExpected {
+			t.Fatalf("unexpected result; got\n%q\nwant\n%q", buf, sExpected)
+		}
+	}
+	f("123", "123,")
+	f("1+2", "3,")
+	f("1+a", "1,a, (), (),1 + a,")
+	f("f(a<b+1, sum(x) by (y))", "a,b,1, (), (),b + 1, (), (),a < (b + 1),x,by (y),sum(x) by (y),f(a < (b + 1), sum(x) by (y)),")
+	f("x[1s]", "x,x[1s],")
+}
