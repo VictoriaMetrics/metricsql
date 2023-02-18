f56fa70104a20ed872421dcc8d2d18978c6ddf95
Aliaksandr Valialkin <valyala@victoriametrics.com>
2023-02-18 14:33:59 -0800

    transform.go: add range_mad(q) function for calculating median absolute deviation for points per each time series returned by q

--- transform.go
+++ transform.go
@@ -74,6 +74,7 @@ var transformFuncs = map[string]bool{
 	"range_first":                true,
 	"range_last":                 true,
 	"range_linear_regression":    true,
+	"range_mad":                  true,
 	"range_max":                  true,
 	"range_min":                  true,
 	"range_normalize":            true,
